2f2f23332587ffd93dc51659406f0b4b51bb2a12
Colin Alexander <chalexander@me.com>
2019-08-18 16:12:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def square_array(array)
   count = 0
   array_squared = []
   
-  while count < array.length do
-    array_squared[count] = array[count]**2
+  while count < array.length do 
+    binding.pry 
+    #array_squared.push(array[count]**2)
     count++
   end
   
